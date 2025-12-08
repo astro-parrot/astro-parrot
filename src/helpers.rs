553bcd3f0ef194bf599f91654e4882689d14b4d4
Bridiro <ale.bridi15@gmail.com>
2025-12-08 13:56:51 +0100

    feat: macros to easily expand combinations

--- src/helpers.rs
+++ src/helpers.rs
@@ -7,6 +7,37 @@ use common_game::components::{
     },
 };
 
+macro_rules! simple_comb {
+    ($resource:expr, $gen:expr, $cell:expr, { $($variant:ident,)* }) => {
+        match $resource {
+            $(
+                $variant => {
+                    paste::paste! {
+                        $gen.[<make_ $variant:lower>]($cell)
+                            .map(BasicResource::$variant)
+                    }
+                },
+            )*
+        }
+    };
+}
+
+macro_rules! complex_comb {
+    ($req:expr, $comb:expr, $cell:expr, { $($variant:ident,)* }) => {
+        match $req {
+            $(
+                $variant(a, b) => {
+                    paste::paste! {
+                        $comb.[<make_ $variant:lower>](a, b, $cell)
+                            .map(|v| v.to_complex())
+                            .map_err(|(e, a, b)| (e, a.to_generic(), b.to_generic()))
+                    }
+                },
+            )*
+        }
+    };
+}
+
 pub fn try_build_rocket(state: &mut PlanetState) {
     if let Some((_, idx)) = state.full_cell() {
         let _ = state.build_rocket(idx);
@@ -20,14 +51,18 @@ pub fn generate_basic(
 ) -> Option<BasicResource> {
     use BasicResourceType::*;
 
-    let result = match resource {
-        Carbon => generator.make_carbon(cell).map(BasicResource::Carbon),
-        Hydrogen => generator.make_hydrogen(cell).map(BasicResource::Hydrogen),
-        Oxygen => generator.make_oxygen(cell).map(BasicResource::Oxygen),
-        Silicon => generator.make_silicon(cell).map(BasicResource::Silicon),
-    };
-
-    result.ok()
+    simple_comb!(
+        resource,
+        generator,
+        cell,
+        {
+            Hydrogen,
+            Oxygen,
+            Silicon,
+            Carbon,
+        }
+    )
+    .ok()
 }
 
 pub fn combine_complex(
@@ -37,35 +72,17 @@ pub fn combine_complex(
 ) -> Result<ComplexResource, (String, GenericResource, GenericResource)> {
     use ComplexResourceRequest::*;
 
-    match req {
-        Water(h, o) => combinator
-            .make_water(h, o, cell)
-            .map(|w| w.to_complex())
-            .map_err(|(e, h, o)| (e, h.to_generic(), o.to_generic())),
-
-        AIPartner(r, d) => combinator
-            .make_aipartner(r, d, cell)
-            .map(|a| a.to_complex())
-            .map_err(|(e, r, d)| (e, r.to_generic(), d.to_generic())),
-
-        Diamond(c1, c2) => combinator
-            .make_diamond(c1, c2, cell)
-            .map(|d| d.to_complex())
-            .map_err(|(e, c1, c2)| (e, c1.to_generic(), c2.to_generic())),
-
-        Dolphin(w, l) => combinator
-            .make_dolphin(w, l, cell)
-            .map(|d| d.to_complex())
-            .map_err(|(e, w, l)| (e, w.to_generic(), l.to_generic())),
-
-        Life(w, c) => combinator
-            .make_life(w, c, cell)
-            .map(|l| l.to_complex())
-            .map_err(|(e, w, c)| (e, w.to_generic(), c.to_generic())),
-
-        Robot(s, l) => combinator
-            .make_robot(s, l, cell)
-            .map(|r| r.to_complex())
-            .map_err(|(e, s, l)| (e, s.to_generic(), l.to_generic())),
-    }
+    complex_comb!(
+        req,
+        combinator,
+        cell,
+        {
+            Water,
+            AIPartner,
+            Diamond,
+            Dolphin,
+            Life,
+            Robot,
+        }
+    )
 }
